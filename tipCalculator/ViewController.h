8236a56c0cf5e004f8338386d93e89c89d71b3ec
Val <vmiha@yahoo-inc.com>
2016-09-07 16:40:29 -0700

    Initial Commit

---
+++ tipCalculator/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  tipCalculator
+//
+//  Created by Valentin Mihaylov on 9/7/16.
+//  Copyright © 2016 codepath. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
